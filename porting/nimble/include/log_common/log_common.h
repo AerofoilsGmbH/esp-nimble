f59c6b740a005293899aea61a8d543015240a945
timoxd7 <timoxd7@gmx.de>
2023-07-26 17:55:27 +0200

    Added missing include to compile NimBLE on ESP IDF

--- porting/nimble/include/log_common/log_common.h
+++ porting/nimble/include/log_common/log_common.h
@@ -1,3 +1,5 @@
+// clang-format off
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -21,6 +23,7 @@
 #define H_LOG_COMMON_
 
 #include <stdint.h>
+#include "syscfg/syscfg.h"
 #include "log_common/ignore.h"
 
 #ifdef __cplusplus
